7bf0e23e46c7e2b9df4a9371865d20235c661264
koberan <koberan@gmail.com>
2025-04-13 13:33:43 -0400

    day 2

--- code/apicalls.py
+++ code/apicalls.py
@@ -1,26 +1,43 @@
 import requests
 
-# Put your CENT Ischool IoT Portal API KEY here.
+# Put your  CENT Ischool IoT Portal API KEY here.
 APIKEY = "69b378f89bdb4b2d0ef3a396"
 
 def get_google_place_details(google_place_id: str) -> dict:
-    pass # Implement this function
-    
+    header = { 'X-API-KEY': APIKEY }
+    params = { 'place_id': google_place_id }
+    url = "https://cent.ischool-iot.net/api/google/places/details"
+    response = requests.get(url, headers=header, params=params)
+    response.raise_for_status()
+    return response.json()  # Return the JSON response as a dictionary
+
+
 def get_azure_sentiment(text: str) -> dict:
-    pass # Implement this function
+    header = { 'X-API-KEY': APIKEY }
+    data = { "text" : text }
+    url = "https://cent.ischool-iot.net/api/azure/sentiment"
+    response = requests.post(url, headers=header, data=data)
+    response.raise_for_status()
+    return response.json()  # Return the JSON response as a dictionary
 
 def get_azure_key_phrase_extraction(text: str) -> dict:
-    pass # Implement this function
+    header = { 'X-API-KEY': APIKEY }
+    data = { "text" : text }
+    url = "https://cent.ischool-iot.net/api/azure/keyphrasextraction"
+    response = requests.post(url, headers=header, data=data)
+    response.raise_for_status()
+    return response.json()  # Return the JSON response as a dictionary
 
 def get_azure_named_entity_recognition(text: str) -> dict:
-    pass # Implement this function
+    header = { 'X-API-KEY': APIKEY }
+    data = { "text" : text }
+    url = "https://cent.ischool-iot.net/api/azure/entityrecognition"
+    response = requests.post(url, headers=header, data=data)
+    response.raise_for_status()
+    return response.json()  # Return the JSON response as a dictionary
 
 
 def geocode(place:str) -> dict:
-    '''
-    Given a place name, return the latitude and longitude of the place.
-    Written for example_etl.py
-    '''
     header = { 'X-API-KEY': APIKEY }
     params = { 'location': place }
     url = "https://cent.ischool-iot.net/api/google/geocode"
@@ -30,10 +47,6 @@ def geocode(place:str) -> dict:
 
 
 def get_weather(lat: float, lon: float) -> dict:
-    '''
-    Given a latitude and longitude, return the current weather at that location.
-    written for example_etl.py
-    '''
     header = { 'X-API-KEY': APIKEY }
     params = { 'lat': lat, 'lon': lon, 'units': 'imperial' }
     url = "https://cent.ischool-iot.net/api/weather/current"
